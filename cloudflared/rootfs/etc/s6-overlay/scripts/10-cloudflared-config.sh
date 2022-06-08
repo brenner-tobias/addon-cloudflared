55dc63890bfabf7b20806dc46b679260a396059c
Elcajon <github@max-venz.de>
2022-06-08 08:40:49 +0200

    Update Base image / migrate to s6-overlay v3

--- cloudflared/rootfs/etc/s6-overlay/scripts/10-cloudflared-config.sh
+++ cloudflared/rootfs/etc/s6-overlay/scripts/10-cloudflared-config.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # shellcheck disable=SC2207
 # ==============================================================================
 # Home Assistant Add-on: Cloudflared
